9139f622db82da6c965f3a8473539b7b71c87f80
ArthurRaimann <araimann25@gmail.com>
2023-02-25 19:19:14 +0100

    bis inkl. 6.88

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
-  return <div className="App">Books App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title: string) => {
+    console.log('need to add book with title: ', title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -1,5 +1,32 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from 'react';
+
+function BookCreate({ onCreate }: any) {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event: any) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>New Book Title</label>
+        <input
+          value={title}
+          type="text"
+          onChange={handleChange}
+          placeholder="title"
+        />
+        <button>Create</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
